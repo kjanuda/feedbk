37b575aa27f7141c4fbcdcc2757fba2d55ec01f8
kjanuda <janudakodi@gmail.com>
2025-09-19 14:28:14 +0530

    Fix start script

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(passport.initialize());
 app.use('/api/recommendations', recommendationRoutes);
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log('Connected to MongoDB');
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
